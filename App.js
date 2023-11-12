e6bd96ab0bda2b30bf79a8ee9f989720d3831454
Vithushan Logeswaran <lvithu123@outlook.com>
2023-11-12 08:15:40 +0530

    basic config added

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> HELLO </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// Optional Component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
